21a91af159583ca9628ea3881df66d64b908ebb8
ICE <ice@lab05m24.nrc.ice.ufjf.br>
2019-06-12 19:38:41 -0300

    Commit dos modelos usados

---
+++ src/main/java/br/ufjf/dcc193/model/Avaliador.java
@@ -0,0 +1,97 @@
+package zzzzz.com.example.zzzz.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+import java.util.List;
+
+
+import javax.persistence.JoinColumn;
+
+import javax.persistence.OneToMany;
+
+
+@Entity
+public class Avaliador {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String nome;
+    private String email;
+    
+    @OneToMany
+    @JoinColumn
+    private List<Categoria> categoria;
+
+
+    public Avaliador() {
+
+    }
+
+    public Avaliador(String nome, String email, Categoria categoria, Double horasDeAtividade, Date dataDeInicio, Date dataDeFim) {
+        this.nome = nome;
+        this.email = email;
+        this.categoria.add(categoria);
+           
+    }
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the categoria
+     */
+    public List<Categoria> getCategoria() {
+        return categoria;
+    }
+
+    /**
+     * @param categoria the categoria to set
+     */
+    public void setCategoria(List<Categoria> categoria) {
+        this.categoria = categoria;
+    }
+ 
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
---
+++ src/main/java/br/ufjf/dcc193/model/Categoria.java
@@ -0,0 +1,66 @@
+package zzzzz.com.example.zzzz.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+
+
+@Entity
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String nome;
+    
+    
+    @JoinColumn
+    @ManyToOne()
+    private Avaliador avaliador;
+
+
+
+    public Categoria() {
+
+    }
+
+    public Categoria(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+ 
+}
