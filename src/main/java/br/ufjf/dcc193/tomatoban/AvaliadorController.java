3394bed6723bbc2c9553b5628357783dace9d943
Cassio Reis <cassio_2000@hotmail.com>
2019-06-13 18:04:06 -0700

    Criacao das funcoes de editar e remover categorias, avaliadores

--- src/main/java/br/ufjf/dcc193/tomatoban/AvaliadorController.java
+++ src/main/java/br/ufjf/dcc193/tomatoban/AvaliadorController.java
@@ -10,6 +10,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.ufjf.dcc193.tomatoban.model.Avaliador;
@@ -65,4 +67,41 @@ public class AvaliadorController {
         mv.addObject("avaliadores", avaliadores);
         return mv;
     }
+
+
+    @RequestMapping(value = { "/editar" }, method = RequestMethod.GET)
+    public ModelAndView editar(@RequestParam("id") Long id) {
+        Avaliador avaliador = avaliadorRepository.getOne(id);
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("avaliador", avaliador);
+        mv.setViewName("editar-avaliador.html");
+        return mv;
+
+}
+
+  
+@RequestMapping(value = { "/deletar" }, method = RequestMethod.GET)
+    public ModelAndView deletar(@RequestParam("id") Long id) {
+        avaliadorRepository.deleteById(id);
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("redirect:/avaliador/listar-avaliadores.html");
+        return mv;
+
+}
+
+@PostMapping(value="/editar-avaliadores.html")
+public ModelAndView editar(@RequestParam(value = "id", required = true) Long id ,@Valid Avaliador avaliador, BindingResult binding){
+        ModelAndView mv = new ModelAndView();
+        if(binding.hasErrors()){
+            mv.setViewName("criar-categoria");
+            mv.addObject("avaliador", avaliador);
+            return mv;
+        }
+        avaliador.setId(id);
+        avaliadorRepository.save(avaliador);
+        mv.setViewName("redirect:/trabalhos/listar-avaliadores.html");
+        return mv;
+}
+
+
 }
